c82365b1b15b8d3c7d3431063d48cf2ecb79bad2
Kenneth Bradley <k_bnet570@verizon.net>
2016-05-01 09:59:45 -0400

    Added database connection script to application

---
+++ CMS/includes/db.php
@@ -0,0 +1,21 @@
+<?php
+//assign all parameters for mysql_connect() to variables
+
+$db['db_host'] = 'localhost';
+$db['db_user'] = 'root';
+$db['db_pass'] = '';
+$db['db_name'] = 'cms';
+
+//Convert each variable to constant
+
+foreach($db as $key => $value){ define(strtoupper($key), $value); }
+
+//	Create connection to database using mysql_connect(localhost, root, password, database name)
+
+$connection = mysql_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+if(!$connection) { 
+	die("Database connection failed"); 
+} else {
+	echo "Connect to CMS database";
+}
+?>
