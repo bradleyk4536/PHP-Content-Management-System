57d4674b0bc9a6510de2b3819c151204f4bc715b
Kenneth Bradley <k_bnet570@verizon.net>
2016-05-02 07:55:50 -0400

    added post query to index page

--- CMS/index.php
+++ CMS/index.php
@@ -8,45 +8,60 @@
 <?php include "partials/navigation.php"; ?>    
 
     <!-- Page Content -->
-    <div class="container">
+<div class="container">
 
-        <div class="row">
+  <div class="row">
 
-            <!-- Blog Entries Column -->
-            <div class="col-md-8">
+			<!-- Blog Entries Column -->
+			<div class="col-md-8">
 
-                <h1 class="page-header">
-                    Page Heading
-                    <small>Secondary Text</small>
-                </h1>
+					<?php
+						$query = "SELECT * FROM posts";
+						$select_all_post_query = mysqli_query($connection, $query);
+						while($row = mysqli_fetch_assoc($select_all_post_query)) {
+						$post_title = $row['post_title'];
+						$post_author = $row['post_author'];
+						$post_date = $row['post_date'];
+						$post_image = $row['post_image'];
+						$post_content = $row['post_content'];
 
-                <!-- First Blog Post -->
-                <h2>
-                    <a href="#">Blog Post Title</a>
-                </h2>
-                <p class="lead">
-                    by <a href="index.php">Start Bootstrap</a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
-                <hr>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-                <hr>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-                <hr>
-            </div>
+					?>
 
-            <!-- Blog Sidebar Widgets Column -->
-            
-            <?php include "partials/sidebar.php"; ?> 
-            
-            
-						<!--  End side bar-->
+					<h1 class="page-header">
+							Page Heading
+							<small>Secondary Text</small>
+					</h1>
 
-        </div>
+					<!-- First Blog Post -->
+					<h2>
+							<a href="#"> <?php echo $post_title ?> </a>
+					</h2>
+					<p class="lead">
+							by <a href="index.php"> <?php echo $post_author ?> </a>
+					</p>
+					<p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date ?> </p>
+					<hr>
+					<img class="img-responsive" src="http://placehold.it/900x300" alt="">
+					<hr>
+					<p> <?php echo $post_content ?> </p>
+					<a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+					<hr>							
+				<?php	} ?>
+			</div>
+
+				<!-- Blog Sidebar Widgets Column -->
+
+				<?php include "partials/sidebar.php"; ?> 
+
+
+				<!--  End side bar-->
+
+  </div>
         <!-- /.row -->
 
         <hr>
         <!--Footer located in includes folder-->
         
 				<?php include "/partials/footer.php"; ?>
+				
+				
