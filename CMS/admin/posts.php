8cb6e48c481c7513cfe84cb5ae6a555ddff00e35
Kenneth Bradley <k_bnet570@verizon.net>
2016-05-04 08:51:29 -0400

    Added post create form to admin section of application.

--- CMS/admin/posts.php
+++ CMS/admin/posts.php
@@ -10,50 +10,31 @@
 					Welcome To Admin
 					<small>Author</small>
         </h1>
+<!--        conditional view all post from partials-->
 
-					<table class="table table-bordered table-hover">
-						<thead>
-							<tr>
-								<th>Id</th>
-								<th>Author</th>
-								<th>Title</th>
-								<th>Category</th>
-								<th>Status</th>
-								<th>Image</th>
-								<th>Tags</th>
-								<th>Comments</th>
-								<th>Date</th>
-							</tr>
-						</thead>
-						<tbody>
-				<?php
-					$query = "SELECT * FROM posts";
-					$select_posts = mysqli_query($connection, $query);
-					while($row = mysqli_fetch_assoc($select_posts)) {
-					$post_id = $row['post_id'];
-					$post_author = $row['post_author'];
-					$post_title = $row['post_title'];
-					$post_category_id = $row['post_category_id'];
-					$post_status = $row['post_status'];
-					$post_image = $row['post_image'];
-					$post_tags = $row['post_tags'];
-					$post_comments = $row['post_comment_count'];
-					$post_date = $row['post_date'];
-						echo "<tr>";
-						echo "<td>{$post_id}</td>";
-						echo "<td>{$post_author}</td>";
-						echo "<td>{$post_title}</td>";
-						echo "<td>{$post_category_id}</td>";
-						echo "<td>{$post_status}</td>";
-						echo "<td><img width=100 src='../images/$post_image'></td>";
-						echo "<td>{$post_tags}</td>";
-						echo "<td>{$post_comments}</td>";
-						echo "<td>{$post_date}</td>";
-						echo "</tr>";
+			<?php
+
+					if(isset($_GET['source'])) {
+
+						$source = $_GET['source'];
+
+					} else {
+
+						$source = '';
+
 					}
-				?>
-						</tbody>
-					</table>
+						switch($source) {
+
+							case 'add_post';
+								include "partials/add_posts.php";
+							break;
+
+
+							default: 	include "partials/view_all_posts.php";
+						}
+
+			?>
+
 				</div>
 			</div>
 				<!-- /.row -->
