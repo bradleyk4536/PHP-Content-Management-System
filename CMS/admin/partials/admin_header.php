8dfde2728bb1d8f9817590ebca26a6b7af844426
Kenneth Bradley <k_bnet570@verizon.net>
2016-05-10 08:25:24 -0400

    Added TnyMCE wysiwyg editor to application

--- CMS/admin/partials/admin_header.php
+++ CMS/admin/partials/admin_header.php
@@ -44,7 +44,9 @@
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-
+<!--    used to display wysiwyg edit to application-->
+		<script src="http://cdn.tinymce.com/4/tinymce.min.js"></script>
+		<script src="js/scripts.js"></script>
 </head>
 
 <body>
