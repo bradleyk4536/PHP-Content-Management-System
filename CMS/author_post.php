e213d0f61e308ed4aacd2e7e5eaf96eab2601437
Kenneth Bradley <k_bnet570@verizon.net>
2016-05-12 07:08:06 -0400

    Added feature to relate post to their authors

--- CMS/author_post.php
+++ CMS/author_post.php
@@ -1,71 +1,48 @@
 <!--DOCTYPE head located in includes folder-->
 <?php include "database/db.php"; ?>
 <?php include "partials/header.php"; ?>
-
-
 <!--		Navigation located in includes folder -->
-    
-<?php include "partials/navigation.php"; ?>    
-
+<?php include "partials/navigation.php"; ?>
     <!-- Page Content -->
 <div class="container">
-
   <div class="row">
-
 			<!-- Blog Entries Column -->
 			<div class="col-md-8">
-
 					<?php
-						$query = "SELECT * FROM posts ";
+				if(isset($_GET['p_id'])) {
+					$the_post_id = $_GET['p_id'];
+					$the_post_author = $_GET['author'];
+				}
+						$query = "SELECT * FROM posts WHERE post_author = '{$the_post_author}' ";
 						$select_all_post_query = mysqli_query($connection, $query);
 						while($row = mysqli_fetch_assoc($select_all_post_query)) {
-						$post_id = $row['post_id'];
 						$post_title = $row['post_title'];
 						$post_author = $row['post_author'];
 						$post_date = $row['post_date'];
 						$post_image = $row['post_image'];
-
-//							truncate text using substr function
-						$post_content = substr($row['post_content'],0,100);
-
-						$post_status = $row['post_status'];
-//
-							if($post_status == 'published') {}
-								?>
+						$post_content = $row['post_content'];
+					?>
 					<!-- First Blog Post -->
 					<h2>
-							<a href="post.php?p_id=<?php echo $post_id; ?>"> <?php echo $post_title ?> </a>
+							<a href="#"> <?php echo $post_title ?> </a>
 					</h2>
-					<p class="lead">
-							by <a href="author_post.php?author=<?php echo $post_author ?>&p_id=<?php echo $post_id ?>"> <?php echo $post_author ?> </a>
-					</p>
+					<p class="lead">Posts by <?php echo $post_author ?></p>
 					<p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date ?> </p>
 					<hr>
-					<a href="post.php?p_id=<?php echo $post_id; ?>">
 					<img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
-					</a>
 					<hr>
 					<p> <?php echo $post_content ?> </p>
-					<a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id; ?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-					<hr>							
-				<?php	}  ?>
-
-
+					<hr>
+				<?php	} ?>
 			</div>
-
 				<!-- Blog Sidebar Widgets Column -->
-
-				<?php include "partials/sidebar.php"; ?> 
-
-
+				<?php include "partials/sidebar.php"; ?>
 				<!--  End side bar-->
-
   </div>
         <!-- /.row -->
-
         <hr>
-        <!--Footer located in includes folder-->
-        
-				<?php include "/partials/footer.php"; ?>
-				
-				
+<!--Footer located in includes folder-->
+
+<?php include "/partials/footer.php"; ?>
+
+
