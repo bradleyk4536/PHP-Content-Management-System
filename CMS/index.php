f60db890414be6f3653db6a6c651feaf7f4f1a19
Kenneth Bradley <k_bnet570@verizon.net>
2016-05-17 11:04:47 -0400

    Added no post available functionality to post

--- CMS/index.php
+++ CMS/index.php
@@ -1,27 +1,23 @@
 <!--DOCTYPE head located in includes folder-->
 <?php include "database/db.php"; ?>
 <?php include "partials/header.php"; ?>
-
-
 <!--		Navigation located in includes folder -->
-    
-<?php include "partials/navigation.php"; ?>    
-
+<?php include "partials/navigation.php"; ?>
     <!-- Page Content -->
 <div class="container">
-
   <div class="row">
-
 			<!-- Blog Entries Column -->
 			<div class="col-md-8">
-
 					<?php
 //				pagination system
 //				get total posts
-						$post_count_query = "SELECT * FROM posts ";
+						$post_count_query = "SELECT * FROM posts WHERE post_status = 'published' ";
 						$find_count = mysqli_query($connection, $post_count_query);
 						$count = mysqli_num_rows($find_count);
-
+						if($count < 1) {
+								echo "<div class='alert alert-warning alert-dismissable text-center' role='alert'>
+			  				<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button><strong>NO BLOG POST AVAILABLE</strong></div>";
+						} else {
 						$count = ceil($count / 5);
 						if(isset($_GET['page'])) {
 							$page = $_GET['page'];
@@ -33,7 +29,6 @@
 						} else {
 							$page_1 = ($page * 5) - 5;
 						}
-
 						$query = "SELECT * FROM posts LIMIT $page_1, 5 ";
 						$select_all_post_query = mysqli_query($connection, $query);
 						while($row = mysqli_fetch_assoc($select_all_post_query)) {
@@ -42,13 +37,10 @@
 						$post_user = $row['post_user'];
 						$post_date = $row['post_date'];
 						$post_image = $row['post_image'];
-
 //							truncate text using substr function
 						$post_content = substr($row['post_content'],0,100);
 
 						$post_status = $row['post_status'];
-//
-							if($post_status == 'Published') {
 								?>
 					<!-- First Blog Post -->
 					<h2>
@@ -66,42 +58,28 @@
 					<p> <?php echo $post_content ?> </p>
 					<a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id; ?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 					<hr>							
-				<?php	} } ?>
-
-
+				<?php
+						}
+						}
+				?>
 			</div>
-
 				<!-- Blog Sidebar Widgets Column -->
-
-				<?php include "partials/sidebar.php"; ?> 
-
-
-				<!--  End side bar-->
-
+				<?php include "partials/sidebar.php"; ?>
   </div>
-        <!-- /.row -->
         <hr>
-
 <!--        PAGINATION-->
      <ul class="pager">
     <?php
 			 for($i = 1; $i<= $count; $i++ ) {
-
 				 if($i == $page) {
-
 					 echo "<li><a class='active_link' href='index.php?page=$i'>{$i}</a></li>";
 				 } else {
-
 					 echo "<li><a href='index.php?page=$i'>{$i}</a></li>";
 				 }
-
 			 }
 		?>
      </ul>
-
-
         <!--Footer located in includes folder-->
-        
 				<?php include "partials/footer.php"; ?>
 				
 				
