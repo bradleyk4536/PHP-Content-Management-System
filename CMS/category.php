ffe84b25ae6f49ed2a2a171279486019200c1dda
Kenneth Bradley <k_bnet570@verizon.net>
2016-05-17 11:32:59 -0400

    added functionality if no post are available for a selected category

--- CMS/category.php
+++ CMS/category.php
@@ -19,11 +19,20 @@
 
 //				Then this page is displayed the $_GET super global captures the URL category id and then it is saved into the $post_category_id variable. It is then sent to the $query where it selects all the post from a given category
 
-				if(isset($_GET['category'])){ $post_category_id = $_GET['category']; }
+				if(isset($_GET['category'])){
+
+					$post_category_id = $_GET['category'];
 
 
-						$query = "SELECT * FROM posts WHERE post_category_id = $post_category_id ";
+						$query = "SELECT * FROM posts WHERE post_category_id = $post_category_id  AND post_status = 'published' ";
 						$select_all_post_query = mysqli_query($connection, $query);
+
+						if(mysqli_num_rows($select_all_post_query) < 1) {
+
+							echo "<div class='alert alert-warning alert-dismissable text-center' role='alert'>
+			  				<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button><strong>NO BLOG POST AVAILABLE FOR SELECT CATEGORY</strong></div>";
+						} else {
+
 						while($row = mysqli_fetch_assoc($select_all_post_query)) {
 						$post_id = $row['post_id'];
 						$post_title = $row['post_title'];
@@ -50,7 +59,11 @@
 					<p> <?php echo $post_content ?> </p>
 					<a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 					<hr>
-				<?php	} ?>
+				<?php	} } } else {
+
+					header("Location: index.php");
+				}
+				?>
 
 
 			</div>
